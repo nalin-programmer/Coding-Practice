5df8dcc08ac564f2c8ee843e44fae7a2481ff094
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-06-28 16:56:55 +0530

    `CP Practice`

---
+++ GeeksForGeeks/LongestCommonSubstring.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+   public:
+    int longestCommonSubstr(string s1, string s2, int n, int m) {
+        // your code here
+        int dp[n + 1][m + 1], ans = 0;
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                ans = max(dp[i][j], ans);
+            }
+        }
+        return ans;
+    }
+};
+lass Solution{
+    public :
+
+        int longestCommonSubstr(string s1, string s2, int n, int m){
+            // your code here
+            int dp[n + 1][m + 1], ans = 0;
+for (int i = 0; i <= n; i++) {
+    dp[i][0] = 0;
+}
+for (int j = 0; j <= m; j++) {
+    dp[0][j] = 0;
+}
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+        if (s1[i - 1] == s2[j - 1]) {
+            dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+            dp[i][j] = 0;
+        }
+        ans = max(dp[i][j], ans);
+    }
+}
+return ans;
+}
+}
+;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr(s1, s2, n, m) << endl;
+    }
+}
